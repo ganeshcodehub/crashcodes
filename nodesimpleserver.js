0f267a07e8f97c8b44add39e194adbff4991e34b
ganeshcodehub <33509301+ganeshcodehub@users.noreply.github.com>
2019-10-07 22:52:43 +0530

    Create nodesimpleserver.js
    Initial file

---
+++ nodesimpleserver.js
@@ -0,0 +1,48 @@
+const http = require('http');
+const path = require('path');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
+  } else if (req.url === '/about') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
+  } else if (req.url === '/about') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'about.html'),
+      (err, content) => {
+        if (err) throw err;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
+  } else if (req.url === '/api/users') {
+    const users = [
+      { id: 101, name: 'Jack Smith', age: 23 },
+      { id: 102, name: 'Arman Jack', age: 35 },
+      { id: 103, name: 'Aaron Peter', age: 40 }
+    ];
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify(users));
+  }
+});
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server is running in ${PORT}`);
+});
